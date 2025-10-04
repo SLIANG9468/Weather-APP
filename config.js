8592f68afc1b1facce245c1200c4c8d4f5f292b7
Sherri Liang-Zhou <sherriliangil@gmail.com>
2025-10-04 06:58:05 -0500

    finish

---
+++ config.js
@@ -0,0 +1,4 @@
+const apiKey = '58316db3219bf8e7478f9e97ae244c7a'; 
+
+export {apiKey}
+ 
--- scripts.js
+++ scripts.js
@@ -1,39 +1,80 @@
-const form = document.getElementById('myForm');
-const highTemp = document.getElementById('highTemp');
-const lowTemp = document.getElementById('lowTemp');
-const forecast = document.getElementById('forecast');
-const humidity = document.getElementById('humidity');
+import { apiKey } from "./config.js";
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const city = document.getElementById('searchBar').value;
-    console.log(city);
-    const apiKey = '58316db3219bf8e7478f9e97ae244c7a'; 
+const myForm = document.getElementById('myForm');
 
-    // Fetch weather info from OpenWeatherMap
-    //const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-    const weatherUrl = `http://api.openweathermap.org/geo/1.0/direct?q=Chicago&limit=1&appid=58316db3219bf8e7478f9e97ae244c7a`;
+const myInput = document.getElementById('searchBar');
 
-    console.log(weatherUrl);
-    try {
-        const response = await fetch(weatherUrl);
-        const data = await response.json();
+const cardContainer = document.querySelector('.card-container')
 
-        if (data.cod !== 200) {
-            alert('City not found');
-            return;
+const getWeather = async(city) => {
+
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`);
+  
+    const data = await response.json();
+        console.log(data);
+
+        const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${data[0].lat}&lon=${data[0].lon}&appid=${apiKey}`;
+
+
+        const weather_response = await fetch(weatherUrl);
+        const weather_data = await weather_response.json();
+        console.log(weather_data);
+
+        const weatherData = {
+            city: data[0].name,
+            country: data[0].country,
+            highTemp: Math.round(((weather_data.daily[0].temp.max - 273.15) * 1.8) + 32),
+            lowTemp: Math.round(((weather_data.daily[0].temp.min - 273.15) * 1.8) + 32),
+            humidity: weather_data.daily[0].humidity,
+            forecast: weather_data.daily[0].summary
         }
 
-        // Update your cards
-        highTemp.textContent = `${Math.round(data.main.temp_max)}℉`;
-        lowTemp.textContent = `${Math.round(data.main.temp_min)}℉`;
-        forecast.textContent = data.weather[0].main;
-        humidity.textContent = `${data.main.humidity}%`;
+        return weatherData;
 
-        // Update heading
-        document.querySelector('h2').textContent = `${data.name}, ${data.sys.country}`;
-    } catch (error) {
-        console.error(error);
-        alert('Error fetching weather data');
-    }
+}
+
+const displayWeather = (weatherData) =>{
+    cardContainer.innerHTML = '';
+    const myCard = document.createElement('div');
+    myCard.className = 'my-card';
+
+    myCard.innerHTML = `
+    
+        <h2>${weatherData.city}, ${weatherData.country}</h2>
+        <div class = "card">
+            <div class = "card-header high">High</div>
+            <div class = "card-body">${weatherData.highTemp}</div>
+        </div>
+  
+        <div class = "card">
+            <div class = "card-header low">Low</div>
+            <div class = "card-body">${weatherData.lowTemp}</div>
+        </div>
+
+       <div class = "card">
+            <div class = "card-header forecast">Forecast</div>
+            <div class = "card-body">${weatherData.forecast}</div>
+        </div>
+
+        <div class = "card">
+            <div class = "card-header humidity">Humidity</div>
+            <div class = "card-body">${weatherData.humidity}</div>
+        </div>
+    `
+    cardContainer.appendChild(myCard)
+
+}
+
+myForm.addEventListener('submit', async (event) => {
+
+    event.preventDefault();
+
+
+
+    const city = myInput.value;
+    console.log(city);
+    const weatherData = await getWeather(city);
+    console.log(weatherData);
+    displayWeather(weatherData);
+
 });
